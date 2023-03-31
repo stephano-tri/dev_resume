d028b36cf112cfa8ddcd640ba74b398a20f29099
stephano-tri <mtriumph23@gmail.com>
2023-03-31 19:58:26 +0900

    FEATURE: 프로젝트 리스트 갱신

--- src/component/dispaly/ProjectDisplay.tsx
+++ src/component/dispaly/ProjectDisplay.tsx
@@ -1,28 +1,51 @@
 import styled from "styled-components";
 import React from "react";
 
-const ProjectContent = () => {
+interface Project {
+    header: string
+    subHeader: string
+    stack: string
+    description: string
+}
+
+const projectList: Project[] = [
+    {
+        header : "Saga Pattern",
+        subHeader : "분산 트랜잭션 환경에서의 atomicity를 보장하기 위한 방법",
+        stack : "Spring Boot, Mysql, Kafka",
+        description : "쇼핑몰의 결제 서비스를 시나리오로 여러 실패(잔액 부족, 재고 부족, 결제 실패 등)를 발생시켜 Orchestration 방식을 통한 Saga Pattern을 구현"
+    },
+    {
+        header : "2phase commit",
+        subHeader : "분산 트랜잭션 환경에서의 atomicity를 보장하기 위한 고전적인 방법(여러 노드를 걸친다)",
+        stack : "Spring Boot, Mysql. RabbitMQ",
+        description : "간단한 쇼핑몰 주문 시나리오를 2phase commit을 통해 구현"
+    }
+]
+
+const ProjectContent: React.FunctionComponent<Project> = ({header, subHeader, stack, description}) => {
     return (
         <Container>
             <Header>
-                Saga Pattern
+                {header}
                 <DotContent>
                     .
                 </DotContent>
             </Header>
             <Content>
-                분산 트랜잭션 환경에서의 atomicity를 보장하기 위한 방법
+                {subHeader}
             </Content>
             <Content style={{fontWeight : 'bold', color : '#FCAE1E', fontSize : '20px'}}>
                 Stack
             </Content>
             <Content>
-                Spring Boot, Mysql, Kafka
+                {stack}
             </Content>
             <Content style={{fontWeight : 'bold', color : '#FCAE1E', fontSize : '20px'}}>
                 Description
             </Content>
             <Content>
+                {description}
                 쇼핑몰의 결제 서비스를 시나리오로 여러 실패(잔액 부족, 재고 부족, 결제 실패 등)를 발생시켜 Orchestration 방식을 통한 Saga Pattern을 구현
             </Content>
         </Container>
@@ -32,11 +55,18 @@ const ProjectContent = () => {
 const ProjectDisplay = () => {
     return(
         <Container>
-            <ProjectContent/>
-            <ProjectContent/>
-            <ProjectContent/>
-            <ProjectContent/>
-            <ProjectContent/>
+            {projectList.map((project, index) => {
+                return (
+                    <ProjectContent
+                        key={index}
+                        header={project.header}
+                        subHeader={project.subHeader}
+                        stack={project.stack}
+                        description={project.description}
+                    />
+                )
+              })
+            }
         </Container>
     )
 }
